af35187412b2d0851d66a1fac1503e0632542196
KarolMrozInfoShareStudent <polityk357@gmail.com>
2018-12-19 12:33:48 +0100

    added comments

--- c01/JS/add-content.js
+++ c01/JS/add-content.js
@@ -1,5 +1,7 @@
-var today = new Date();
-var hourNow = today.getHours();
+/*Ten skrypt wyświetla użytkownikowi test powitania wybrany na podstawie aktualnej godziny*/
+
+var today = new Date(); //Utwrzenie nowego oboetu daty
+var hourNow = today.getHours();  //Ustalenie aktualnej godziny 
 var greeting;
 
 if (hourNow > 18) {
