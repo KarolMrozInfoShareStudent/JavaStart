e1da2ecafc8362a73b501ca6fe0a152e7250f505
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 11:05:53 +0100

    usng boolean-variabke.js

---
+++ c03/boolean-variable.js
@@ -0,0 +1,11 @@
+var inStok;
+var shipping;
+
+inStok = true;
+shipping = false;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStok;
+
+var elShip = document.getElementById('shipping');
+elShip.className = shipping
