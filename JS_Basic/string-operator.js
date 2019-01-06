ce21b5e109732238ae7469877580cdd46871631a
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 14:48:04 +0100

    add string-operator.js

---
+++ JS_Basic/string-operator.js
@@ -0,0 +1,8 @@
+var greeting = "Witaj"
+var name = "Karol"
+
+var welcomeMessage = greeting +" " + name + "!";
+
+var ElGreeting = document.getElementById('greeting');
+ElGreeting.textContent = welcomeMessage
+
