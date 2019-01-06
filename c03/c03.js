2d3f70db76f5041e7566a8cdf05ecc05467a23a9
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 09:57:09 +0100

    using string variable

--- c03/c03.js
+++ c03/c03.js
@@ -1,10 +1,10 @@
-// var username;
-// var message;
-// username = ' Karol';
-// message = 'Zopoznaj sie z naszym asortymentem';
+var username;
+var message;
+username = ' Karol';
+message = 'Zopoznaj sie z naszym asortymentem';
 
-// var elName = document.getElementById('name');
-// elName.textContent = username;
+var elName = document.getElementById('name');
+elName.textContent = username;
 
-// var elNote = document.getElementById('note');
-// elNote.textContent= message
+var elNote = document.getElementById('note');
+elNote.textContent= message
