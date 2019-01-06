b3570a4b9b69129d1ccf8fbc3977a4ec65d7c673
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 09:53:26 +0100

    added new folder c03 widh file

---
+++ c03/c03.js
@@ -0,0 +1,10 @@
+// var username;
+// var message;
+// username = ' Karol';
+// message = 'Zopoznaj sie z naszym asortymentem';
+
+// var elName = document.getElementById('name');
+// elName.textContent = username;
+
+// var elNote = document.getElementById('note');
+// elNote.textContent= message
