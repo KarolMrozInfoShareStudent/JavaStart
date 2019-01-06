994772fe212d3a726d399831778fdc0a34828958
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 10:35:38 +0100

    add string-widh-quotes

---
+++ c03/string-widh-quotes.js
@@ -0,0 +1,12 @@
+var title;
+var message;
+
+title = "Oferta specjalana w Biedronka";
+
+message = "<a href=\'sale.html\'>Rabat 25%!</a>";
+
+var elTitleTwo = document.getElementById('title2');
+elTitleTwo.innerHTML = title
+
+var elNote2 = document.getElementById('note2');
+elNote2.innerHTML = message;
