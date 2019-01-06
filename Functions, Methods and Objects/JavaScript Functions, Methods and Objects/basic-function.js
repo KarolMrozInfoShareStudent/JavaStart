93be0eb634ba233e34c55ed55a2d676d21a62ea8
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-06 16:47:19 +0100

    added basic-function.js

---
+++ Functions, Methods and Objects/JavaScript Functions, Methods and Objects/basic-function.js
@@ -0,0 +1,7 @@
+var msg = 'Zapisz sie do naszego newskettera, a otrzymasz 20% rabatu !';
+
+function uptateMesage() {
+    var elMsg = document.getElementById('message');
+    elMsg.innerHTML = msg;
+}
+uptateMesage();
