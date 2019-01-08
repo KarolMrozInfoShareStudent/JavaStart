751203fb2621c76ea2adf6bbf71da8923e91c0a0
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-08 12:28:31 +0100

    added math-objects.js wygenerowanie liczby losowej za pomoca obektu math

---
+++ Functions, Methods and Objects/JavaScript Functions, Methods and Objects/math-objesct.js
@@ -0,0 +1,3 @@
+var randomNum = Math.floor((Math.random() * 10) + 1);
+var el = document.getElementById('info6');
+el.innerHTML = "<p class='opis'>Wygenerowanie liczby losowej za pomoca obiektu math </p>" +"<h2>liczba losowa</h2><p>" + randomNum + "</p>";
