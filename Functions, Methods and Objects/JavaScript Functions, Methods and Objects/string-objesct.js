c70fb86d16981518c399f8994bdc3ad698dfdf30
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-08 10:43:35 +0100

    add string-bjects.js WIP:work with a text string

---
+++ Functions, Methods and Objects/JavaScript Functions, Methods and Objects/string-objesct.js
@@ -0,0 +1,15 @@
+var powiedzenie = 'Home sweet homt';
+
+var msg2 = '<h2>Duze litery</h2><p>'+ powiedzenie.length +'</p>'
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+    msg2 +='<h2></h2><p>' +  '</p>';
+
+var elMsg2 = document.getElementById('info4');
+    elMsg2.innerHTML = msg2;
