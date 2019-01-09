282aedc28a80c41dac5599bc91cc50b5b6411aed
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-09 14:33:41 +0100

    added if-statements.js uzycie polecenia if

---
+++ Decisions & Loops/JS_Decisions-Loops/if-statements.js
@@ -0,0 +1,13 @@
+var score = 100;
+var msg4;
+
+
+if (score >=1){
+
+msg4 = '<h3>Użycie polecenia if</h3>' + '<p>Gratulacje ! </p>'  + 'Przechodzisz do następej rundy.'
+
+
+}
+
+ var el4 = document.getElementById('answer4');
+    el4.innerHTML = msg4
