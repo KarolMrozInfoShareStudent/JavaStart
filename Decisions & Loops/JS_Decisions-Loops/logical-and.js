1ba3622544f05ec16953552f7e41f4026cc0464a
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-09 13:58:15 +0100

    added logical-and.js Uzycie logicznego and

---
+++ Decisions & Loops/JS_Decisions-Loops/logical-and.js
@@ -0,0 +1,13 @@
+var score1 = 8;
+var score2 = 8;
+var pass1 = 6;
+var pass2 = 6;
+
+
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
+
+var msg = '<h3>Użycie logicznego and</h3>' + 'Zaliczenie obu rund: ' + passBoth;
+
+
+var el = document.getElementById('answer2');
+el.innerHTML = msg;
