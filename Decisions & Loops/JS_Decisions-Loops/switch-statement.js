30626715c4a6b5f9f07ca0d957b5175127bcda0b
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-10 11:38:12 +0100

    added switch-statement.js Użycie konstrukcji switch

---
+++ Decisions & Loops/JS_Decisions-Loops/switch-statement.js
@@ -0,0 +1,26 @@
+var msg7;
+var level7 = 2;
+
+switch(level7){
+    case 1:
+        msg7 = "Powodzenia na pierwszym teście";
+        break;
+    case 2:
+        msg7 = "Drugi z trzech jest nieźle";  
+        break;
+    case 3:
+        msg7 = 'Ostatnia runda juz prawie skonczyłeś';
+        break;
+        
+    case 3:
+        msg7 = 'Pamietaj szczacunek to wszystko' 
+        break;
+
+    default:
+        msg7 = 'Powidzenia';
+        breakl 
+};  
+
+    var el7 = document.getElementById('answer7');
+        el7.innerHTML = '<h3>Użycie konstrukcji switch</h3>' + msg7
+
