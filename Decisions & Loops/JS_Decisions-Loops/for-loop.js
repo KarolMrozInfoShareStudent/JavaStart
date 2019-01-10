4f8e4721027d0b826368449bf5e38ca2360a6ecf
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-10 13:21:56 +0100

    added for-loop.js uzyce pętli for

---
+++ Decisions & Loops/JS_Decisions-Loops/for-loop.js
@@ -0,0 +1,11 @@
+var scores8 = [84, 882, 47];
+var arrayLength8 = scores8.length;
+var roundNumber = 0;
+var msg8 = "";
+for (var i = 0; i < arrayLength8; i++) {
+  roundNumber = i + 1;
+
+  msg8 += "Runda " + roundNumber + ": " +  scores8[i] + "<br />";
+}
+
+document.getElementById("answer8").innerHTML = '<h3>Użycie pętli</h3>' + '<br / >' + msg8;
