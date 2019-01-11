f5a99fdc6090d2d60557cde32e49431cb65b670e
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-11 16:04:09 +0100

    Pobieranie elementu na podstawie wartości atrybutu class metoda detElementsByClassName

--- Document Object Model/document_Objecy_Model.js
+++ Document Object Model/document_Objecy_Model.js
@@ -1,2 +1,13 @@
-var el = document.getElementById('one')
-    el.className = 'cool';
+// //Pobranie elementu na podstawie wartości atrybutu id
+// var el = document.getElementById('one')
+//     el.className = 'cool';
+
+
+//Pobieranie elementu na podstawie wartości atrybutu class
+
+    var elements = document.getElementsByClassName('hot');
+        //Wyszukanie elementow o klasie hot
+        if(elements.length > 1){ //jeśli znajdą znalezione conajmniej dwa
+            var el = elements[2]; // z kolekcji NodeList wybierz trzeci 
+                el.className = 'cool' //zmiaana warosci atrybutu class wybranego elementu
+        }
