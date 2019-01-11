c5847857e546c82e4a704eeb9156622bac01f714
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-11 16:24:36 +0100

    Pobranie elementu na podstawie wartości nazwy znacznieka metoda gerElementByTagName()

--- Document Object Model/document_Objecy_Model.js
+++ Document Object Model/document_Objecy_Model.js
@@ -5,9 +5,20 @@
 
 //Pobieranie elementu na podstawie wartości atrybutu class
 
-    var elements = document.getElementsByClassName('hot');
-        //Wyszukanie elementow o klasie hot
-        if(elements.length > 1){ //jeśli znajdą znalezione conajmniej dwa
-            var el = elements[2]; // z kolekcji NodeList wybierz trzeci 
-                el.className = 'cool' //zmiaana warosci atrybutu class wybranego elementu
+    // var elements = document.getElementsByClassName('hot');
+    //     //Wyszukanie elementow o klasie hot
+    //     if(elements.length > 1){ //jeśli znajdą znalezione conajmniej dwa
+    //         var el = elements[2]; // z kolekcji NodeList wybierz trzeci 
+    //             el.className = 'cool' //zmiaana warosci atrybutu class wybranego elementu
+    //     }
+
+
+//Pobranie elementu na podstawie wartości nazwy znacznieka
+
+    var elements = document.getElementsByTagName('li');
+        
+        if(elements.length > 1){
+            var el = elements[0];
+                el.className = 'cool'
         }
+   
