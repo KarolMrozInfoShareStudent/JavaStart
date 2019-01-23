a3f216c090855e48e0ecbcd36087d4f51399ae5d
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-23 15:01:46 +0100

    ustalenie połozenia funkcja showPosition() otrzymuje parametr event odwołujacy sie do obiektu event.Polozenie kurusora jest odczytywane z właściwości wymienionego obiektu event

--- Events_Zdarzenia/events.js
+++ Events_Zdarzenia/events.js
@@ -104,5 +104,25 @@ let elNote = document.createElement('div');
 
     
 const elClose = document.getElementById('close');
-      elClose.addEventListener('click', dismissNote, false);    
+      elClose.addEventListener('click', dismissNote, false);
+      
+//////////////////////////////////////////////////////////////////////////////      
 
+var sx = document.getElementById('sx'); 
+var sy = document.getElementById('sy');
+var px = document.getElementById('px');
+var py = document.getElementById('py');
+var cx = document.getElementById('cx');
+var cy = document.getElementById('cy'); 
+
+function showPosition(event) {  
+  sx.value = event.screenX;  
+  sy.value = event.screenY; 
+  px.value = event.pageX;
+  py.value = event.pageY;           
+  cx.value = event.clientX;
+  cy.value = event.clientY;
+}
+
+var elPosition = document.getElementById('body')  ;
+    elPosition.addEventListener('mousemove', showPosition, false)
