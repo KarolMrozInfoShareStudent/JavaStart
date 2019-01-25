397f108fbc72349a1e57d79acc8e5d6fce51e48f
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-25 10:16:43 +0100

    Petle JQ w przypadku jQuery gdy selektor zwraca wiele elemntów to wszystkie mozna uaktualnic za pomoca jednej metody nie ma koniecznosci uzycia petli

--- Jquery/Jquery.js
+++ Jquery/Jquery.js
@@ -1,5 +1,13 @@
-$(':header').addClass('headline');
-$('li:lt(3)').hide().fadeIn(1500);
-$('li').on('click', function() {
-  $(this).remove();
-});
+// $(':header').addClass('headline');
+// $('li:lt(3)').hide().fadeIn(1500);
+// $('li').on('click', function() {
+//   $(this).remove();
+// });
+
+
+//----------------------------------------------------------//
+//Pętle
+
+$('li em').addClass('seasonal');
+$('li.hot').addClass('favorite');
+
