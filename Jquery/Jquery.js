b710ae8c817bca3efc4e84b7f35469d0d5d2a951
KarolMrozInfoShareStudent <polityk357@gmail.com>
2019-01-28 10:51:09 +0100

    Praca z formularzem sieciowym, dodanie html

--- Jquery/Jquery.js
+++ Jquery/Jquery.js
@@ -186,9 +186,11 @@
 //   });
 
 //----------uzycie wartosci indeksu----
-$(function(){
-  $('li:lt(2)').removeClass('hot');
-  $('li').eq(0).addClass('complete');
-  $('li:gt(2)').addClass('cool')
-});
+// $(function(){
+//   $('li:lt(2)').removeClass('hot');
+//   $('li').eq(0).addClass('complete');
+//   $('li:gt(2)').addClass('cool')
+// });
  
+//----praca z formularzem sieciowym---
+
