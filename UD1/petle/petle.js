40cc0da731716013714a0fcc5190a01440dbcff6
MrozKarol <polityk357@gmail.com>
2019-02-20 20:38:25 +0100

    uzycue petli w petli

--- UD1/petle/petle.js
+++ UD1/petle/petle.js
@@ -1,27 +1,16 @@
-// petle
+// petle w petli
 
-var produkty = 
-[
-    "JavaScript",
-    "Java",
-    "CSS",
-    "Python",
-    "React",
-    "JsX"
-];
-person = {
-    imie: "Arek",
-    nazwisko: "Włodarczyk",
-    wiek: "26"
- };
- 
- var kursyProgramowania = document.getElementById("kursyProgramowania").getElementsByTagName("li");
- 
- 
-for (var property in kursyProgramowania)
+var tabliczkaMnozenia = "<table>";
+
+for (var i =1; i <= 10 ; i++)
 {
-    if (typeof(kursyProgramowania[property]) !=="object")
-    break;
-    alert(kursyProgramowania[property].innerHTML)
-}
+    tabliczkaMnozenia += "<tr>"
+    for(var j = 1; j <=10; j++)
+    tabliczkaMnozenia += "<td>" + i * j + "</td> ";
 
+    tabliczkaMnozenia += "</tr>"
+}
+tabliczkaMnozenia += "</table>";
+var rezultat = document.getElementById("rezultat");
+
+rezultat.innerHTML = tabliczkaMnozenia;
