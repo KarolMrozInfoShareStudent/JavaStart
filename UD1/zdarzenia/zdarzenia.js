41a667e7cbe19c9c89ba3940668739dc4b988642
MrozKarol <polityk357@gmail.com>
2019-02-20 21:23:15 +0100

    'this' - odwoływanie się do właściwości elementu, który wywołał zdarzenie

--- UD1/zdarzenia/zdarzenia.js
+++ UD1/zdarzenia/zdarzenia.js
@@ -1,8 +1,15 @@
 
-var test = document.getElementById("test");
 
-
-
-test.onmouseover = function(){
-    alert("jojo")
+function zmienKolor()
+{
+    this.className = "zmienKolorRed";
 }
+function zmienKolor2()
+{
+    this.className = "zmienKolorGreen";  
+}
+
+var test = document.getElementById("test");
+test.onmouseover = zmienKolor;
+test.onmouseleave = zmienKolor2;
+  
