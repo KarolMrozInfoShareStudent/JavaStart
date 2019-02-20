ec16445f8bfb8895b4a3eb24443653960cf6dadd
MrozKarol <polityk357@gmail.com>
2019-02-20 13:38:44 +0100

    Klasa- w sktocie to forma do wytwarzania obiktow. ta forna sluzy do zebrania obiektow w jedna 'klase'. daje mozliwosc stworzenia z tej formy wielu nowych rozniacych sie minimalnie od siebie obiektow, ale bedacych dalej do siebie podobnych cechami i metodami.

---
+++ UD1/obiekty/obiekty.js
@@ -0,0 +1,19 @@
+var div = document.getElementById('one');
+
+div.innerHTML = "nowa treść";
+
+function person(imie, nazwisko, wiek)
+{
+    this.name = imie;
+    this.surname = nazwisko;
+    this.age = wiek;
+    this.toString =function()
+    {
+        return this.name + " " + this.surname + " " + this.age;
+    };
+}
+var x = new person("Karol", "Mróz", 5);
+var y = new person("Stefan", "Kozaki", "55")
+var z = new person("Joanna", "Dzonski", "30" )
+
+div.innerHTML = x + "<br>" + y + "<br>" + z
