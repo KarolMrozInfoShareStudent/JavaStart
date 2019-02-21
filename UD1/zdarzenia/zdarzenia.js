33c705856f42b94d695d32364d4739100b68e468
MrozKarol <polityk357@gmail.com>
2019-02-21 12:24:00 +0100

    uzycue window.event

--- UD1/zdarzenia/zdarzenia.js
+++ UD1/zdarzenia/zdarzenia.js
@@ -1,42 +1,36 @@
-// addEventListener
+// /*
+//     altKey, ctrlKey, shiftKey - czy podczas wywołania eventu były wciśnięte klawisze alt, ctrl, shift?
+//     button - które przyciski myszy zostały kliknięte (nie działa w każdym evencie)
+//     clientX, clientY - w którym miejscu jest mycha względem 0,0
+//     keyCode - zwraca w postaci liczby jaki klawisz został wciśnięty, String.fromCharCode(e.keyCode) zwróci jaka to wartość
+//     target.tagName - nazwa tagu elementu który wywołał zdarzenie wspierane w każdej przeglądarce prócz IE 6-8 
+//                      użyj srcElement dla IE na starcie można napisać: var srcElement = e.target ? e.target : e.srcElement;
 
-function plusSize()
-{   
-    var fontSize = parseInt(window.getComputedStyle(test).fontSize);
-    test.style.fontSize = (++fontSize) + "px";
-    if(fontSize === 25 ){
+function wykonaj(event, str)
+{
+    var e =  event || window.event;
+    var srcElement = e.target || e.srcElement;
+    
+    var tmp = document.getElementById("tmp");
+    
+    tmp.innerHTML = e.clientX + " " + srcElement.tagName;
+    
+    var tooltip = document.getElementById("tooltip");
+    
+    tooltip.style.display = "block";
+    
+    tooltip.style.left = e.clientX + 10 + "px";
+    tooltip.style.top = e.clientY + 10 + "px";
+    
+}
+window.onload = function()
+{
+    var test = document.getElementById("test");
+    
+    test.onmousemove = function(event)
+    {
         
-        var a = alert("Maksymalne powiększenie")
-        alert(a)
-        plus.removeEventListener( 'click', plusSize);
-    }else{
-        plus.addEventListener( 'click', plusSize);
-    }
-    
-    
-}
-
-
-function minusSize()
-{
-    var fontSize = parseInt(window.getComputedStyle(test).fontSize);
-    test.style.fontSize = (--fontSize) + "px";
-}
-
-function zmienKolor()
-{
-    test.style.color = "red"
-}
-function zmienKolor2()
-{
-    test.style.color = "green" 
-}
-
-var test = document.getElementById("test");
-var plus = document.getElementById("plus")
-var minus = document.getElementById("minus")
-test.addEventListener('mouseover', zmienKolor)
-test.addEventListener('mouseout', zmienKolor2)
-plus.addEventListener( 'click', plusSize);
-minus.addEventListener('click', minusSize)
-
+        wykonaj(event, this.tagName);
+    };
+   
+};
