3bc021fe5ad4bcbba1f9d5168cd7950a3aea89e0
MrozKarol <polityk357@gmail.com>
2019-02-22 15:10:59 +0100

    dodanie warunku do pojawiania sie i znikania przycisku

--- UD1/cwiczenie_v_01/cwiczenie_v_01.js
+++ UD1/cwiczenie_v_01/cwiczenie_v_01.js
@@ -3,15 +3,24 @@ window.onload = function (){
     
 
     window.onscroll = function()
-    {
+    {   
+        var toTopButton = document.getElementById("topButton");
         var testaxisY = document.getElementById("axisY");
+
+        if(window.pageYOffset > 300)
+        {
+            toTopButton.style.display = "block"
+        }else{
+            toTopButton.style.display = "none"
+        }
         testaxisY.innerText = window.pageYOffset + " px"
-    }
+
+    };
 
 
     toTopButton.onclick = function()
     {
-        window.scrollBy(0, 100)
+        window.scrollBy(0, -1 * window.pageYOffset)
         // console.log(pageYOffset)
     };
 };
