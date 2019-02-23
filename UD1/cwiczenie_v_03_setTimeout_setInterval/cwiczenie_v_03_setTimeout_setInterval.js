fe9403117e8637f3f424cda5a4cfa9bc44284046
MrozKarol <polityk357@gmail.com>
2019-02-23 18:11:25 +0100

    dodanie odliczania do zera

--- UD1/cwiczenie_v_03_setTimeout_setInterval/cwiczenie_v_03_setTimeout_setInterval.js
+++ UD1/cwiczenie_v_03_setTimeout_setInterval/cwiczenie_v_03_setTimeout_setInterval.js
@@ -1,8 +1,18 @@
 // setTimeout - metoda ktora wykonuje operacje po okreslonym czasie 
 // setInterval - metoda ktora wykonuje operacje co interwał (co okreslony przez nas czas w nieskonczoność)
 
-
-
+function stopWatch(wynik, liczba)
+{
+    wynik.innerHTML = --liczba;
+    if(liczba <= 0)
+    return
+    // to samo tylko pomniejszone o liczbe -
+    setTimeout(function()
+    {
+        stopWatch(wynik, liczba)
+    },1000
+    )
+}
 
 window.onload = function(){
     var poczatkowaWartosc = document.querySelector("#poczatkowaWartosc");
@@ -10,9 +20,17 @@ window.onload = function(){
     var zatrzymajStoper = document.getElementById("stop");
     var wynik = document.getElementById("wynik");
 
-    statStoper.onclick = function(){
-        var poczatkowaWartosc = document.querySelector("#poczatkowaWartosc").value;
-        wynik.innerHTML = poczatkowaWartosc
-    }
+    
 
+    statStoper.onclick = function(){
+        var poczatkowaWartosc = document.getElementById("poczatkowaWartosc").value;
+        wynik.innerHTML = poczatkowaWartosc;
+        setTimeout(function()
+        {
+            stopWatch(wynik, poczatkowaWartosc)
+        }
+        ,1000);
+        
+    
+    };
 }
