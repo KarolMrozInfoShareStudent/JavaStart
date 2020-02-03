f18ffa96a420fe144e660c2e075951603181fbbe
MrozKarol <polityk357@gmail.com>
2020-02-03 19:14:41 +0100

    many passwords and messages

--- JS Returning/Projekty/5-password mesage/main.js
+++ JS Returning/Projekty/5-password mesage/main.js
@@ -1,24 +1,24 @@
 const input = document.getElementById('pass');
 const div = document.querySelector('.message')
-const password = 'user';
-const message = 'dziła dziła działa'
+const passwords = ['user', 'zima', 'Rege'];
+const messages = ['dziła dziła działa', 'w tym roku nie będzie', 'wygrał życie']
 
+input.addEventListener('input', (e) => {
+    div.textContent = '';
+    const text = e.target.value;
+    console.log(text)
+    passwords.forEach((password, index) => {
+        if (password === text) {
+            div.textContent = messages[index]
+        }
+    })
 
-
-input.addEventListener('input', (e)=>{
-    if(password === e.target.value){
-        div.textContent = message
-    }
-    else{
-        div.textContent = ' '
-    }
 })
 
-
-input.addEventListener('focus', (e)=>{
+input.addEventListener('focus', (e) => {
     e.target.classList.add('active')
 })
 
-input.addEventListener('blur', (e)=>{
+input.addEventListener('blur', (e) => {
     e.target.classList.remove('active')
 })
