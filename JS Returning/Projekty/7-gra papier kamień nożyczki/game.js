bf610f91534eb01626be6a1943c46f2058af15f4
MrozKarol <polityk357@gmail.com>
2020-02-12 19:44:29 +0100

    added computer selection setting

--- JS Returning/Projekty/7-gra papier kamień nożyczki/game.js
+++ JS Returning/Projekty/7-gra papier kamień nożyczki/game.js
@@ -11,6 +11,7 @@ const game ={
     aiHand: '',
 }
 
+
 //pobranie grafik 
 const hands = [...document.querySelectorAll('.select img')]
 
@@ -18,8 +19,25 @@ const handSelection = (e)=> {
     game.playerHand = e.target.dataset.option;
     hands.forEach(hand => hand.style.boxShadow = ''); //zdjęcie z każdego img shadowa
     e.target.style.boxShadow = '0 0 0 4px yellow' 
-
-    console.log(game.playerHand)
+    // console.log(game.playerHand)
 }
+
+const aiChoice = () =>{
+ const aiHand = hands[Math.floor(Math.random()*3)].dataset.option;
+//  console.log(aiHand)
+ return aiHand;
+} 
+//FUNKCJA STERUJĄCA
+const startGame = () =>{
+    if(!game.playerHand){
+       return alert('wybierz dłoń !!!')
+    }
+    game.aiHand = aiChoice()
+}
+
+
 //dodanie zdarzenia na każdy element img
 hands.forEach(hand => hand.addEventListener('click', handSelection) )
+
+
+document.querySelector('.start').addEventListener('click', startGame)
