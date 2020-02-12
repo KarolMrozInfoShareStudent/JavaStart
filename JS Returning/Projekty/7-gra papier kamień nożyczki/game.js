30b964b7ada6eae41570db128167a5ef92d3c57d
MrozKarol <polityk357@gmail.com>
2020-02-12 20:05:40 +0100

    definition of the winning side

--- JS Returning/Projekty/7-gra papier kamień nożyczki/game.js
+++ JS Returning/Projekty/7-gra papier kamień nożyczki/game.js
@@ -1,12 +1,12 @@
 //obiekt przechowujacy podsumowanie gry
-const gameSummary ={
+const gameSummary = {
     number: 0,
     wins: 0,
-    losses:0,
-    draws:0,
+    losses: 0,
+    draws: 0,
 }
 
-const game ={
+const game = {
     playerHand: '',
     aiHand: '',
 }
@@ -15,29 +15,43 @@ const game ={
 //pobranie grafik 
 const hands = [...document.querySelectorAll('.select img')]
 
-const handSelection = (e)=> {
+const handSelection = (e) => {
     game.playerHand = e.target.dataset.option;
     hands.forEach(hand => hand.style.boxShadow = ''); //zdjęcie z każdego img shadowa
-    e.target.style.boxShadow = '0 0 0 4px yellow' 
+    e.target.style.boxShadow = '0 0 0 4px yellow'
     // console.log(game.playerHand)
 }
 
-const aiChoice = () =>{
- const aiHand = hands[Math.floor(Math.random()*3)].dataset.option;
-//  console.log(aiHand)
- return aiHand;
-} 
-//FUNKCJA STERUJĄCA
-const startGame = () =>{
-    if(!game.playerHand){
-       return alert('wybierz dłoń !!!')
+const aiChoice = () => {
+    const aiHand = hands[Math.floor(Math.random() * 3)].dataset.option;
+    //  console.log(aiHand)
+    return aiHand;
+}
+
+const checkResult = (player, ai) => {
+    if (player === ai) {
+        return 'draw'
+    } else if ((player === 'papier' && ai === 'kamień') || (player === 'kamień' && ai === 'nożyczki') || (player === 'nożyczki' && ai === 'papier')) {
+        return 'win'
+    }else{
+        return 'loss'
     }
-    game.aiHand = aiChoice()
+}
+
+//FUNKCJA STERUJĄCA
+const startGame = () => {
+    if (!game.playerHand) {
+        return alert('wybierz dłoń !!!');
+    }
+    game.aiHand = aiChoice();
+
+    const gemeResult = checkResult(game.playerHand, game.aiHand);
+    console.log(gemeResult)
 }
 
 
 //dodanie zdarzenia na każdy element img
-hands.forEach(hand => hand.addEventListener('click', handSelection) )
+hands.forEach(hand => hand.addEventListener('click', handSelection))
 
 
 document.querySelector('.start').addEventListener('click', startGame)
