bbec3ceefdf1476224dc22e5da9e860ebc299c52
MrozKarol <polityk357@gmail.com>
2020-02-16 19:54:10 +0100

    added exercise

--- JS Returning/Projekty/8-typing efekt pisania/ex.js
+++ JS Returning/Projekty/8-typing efekt pisania/ex.js
@@ -6,11 +6,27 @@
 
 const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
-const txt = ['tekst1', 'tekst2', 'tekst3']
+const txt = ['tekst1', 'tekst2', 'tekst3'];
+let activeLetter = 0;
+let activeText = 0;
 
 // Implementacja
 const addLetter = () => {
- // Użyj w środku setTimeout
+    if (activeLetter >= 0) {
+        spnText.textContent += txt[activeText][activeLetter];
+    }
+    activeLetter++;
+    if (activeLetter === txt[activeText].length) {
+        activeText++;
+        if (activeText === txt.length) return;
+
+        return setTimeout(() => {
+            activeLetter = 0;
+            spnText.textContent = '';
+            addLetter();
+        }, 2000)
+    }
+    setTimeout(addLetter, 100)
 }
 
 
@@ -19,6 +35,6 @@ addLetter(); //pierwsze wywołanie
 
 // Animacja kursora (zostaw bez zmian)
 const cursorAnimation = () => {
- spnCursor.classList.toggle('active');
+    spnCursor.classList.toggle('active');
 }
 setInterval(cursorAnimation, 400);
