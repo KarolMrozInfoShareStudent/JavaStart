6dc851f01fbb116e1eb502247fd749ef43616ca2
MrozKarol <polityk357@gmail.com>
2020-02-25 19:33:53 +0100

    checking the user's age and displaying the appropriate message

--- JS Returning/Projekty/10-Closures domkniecia/1-click counter/main.js
+++ JS Returning/Projekty/10-Closures domkniecia/1-click counter/main.js
@@ -11,3 +11,19 @@ const add = (start = 0) => {
 }
 const counter = add()
 document.addEventListener('click', counter)
+
+
+const user = (name='',age) => {
+    let userName = name;
+    let userAge = age;
+    const showName = () => {
+        console.log(`Cześć ${userName}, ${userAge >= 18 ? 'Możesz kupić bronka' : 'Nie możesz kupić bronka'}`)
+    }
+    return showName
+}
+
+const karol = user('Karol', 40);
+const luiza = user('Luiza', 17)
+
+karol()
+luiza()
