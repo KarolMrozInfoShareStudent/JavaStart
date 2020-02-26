ca652516c8580112bb61761d8ad46976a2ddbf14
MrozKarol <polityk357@gmail.com>
2020-02-26 18:56:44 +0100

    use of the arrow function, change of variable declaration

--- UD1/cwiczenie_v_01/cwiczenie_v_01.js
+++ UD1/cwiczenie_v_01/cwiczenie_v_01.js
@@ -1,25 +1,13 @@
-window.onload = function (){
-    var toTopButton = document.getElementById("topButton");
-    
+window.onload = () => {
+    const toTopButton = document.getElementById("topButton");
+    window.onscroll = () => {
+        const toTopButton = document.getElementById("topButton");
+        const testaxisY = document.getElementById("axisY");
 
-    window.onscroll = function()
-    {   
-        var toTopButton = document.getElementById("topButton");
-        var testaxisY = document.getElementById("axisY");
-
-        if(window.pageYOffset > 300)
-        {
-            toTopButton.style.display = "block"
-        }else{
-            toTopButton.style.display = "none"
-        }
-        testaxisY.innerText = window.pageYOffset + " px"
-
+        window.pageYOffset > 300 ? toTopButton.style.display = "block" : toTopButton.style.display = "none"
+        testaxisY.textContent = `${window.pageYOffset} px`
     };
-
-
-    toTopButton.onclick = function()
-    {
+    toTopButton.onclick = () => {
         window.scrollBy(0, -1 * window.pageYOffset)
         // console.log(pageYOffset)
     };
