910a8d92b72da8e0fa9fd66ecd26fc4f08fa5f9b
MrozKarol <polityk357@gmail.com>
2020-02-26 20:08:02 +0100

    using the arrow function

--- UD1/cwiczenie_v_03_setTimeout_setInterval/cwiczenie_v_03_setTimeout_setInterval.js
+++ UD1/cwiczenie_v_03_setTimeout_setInterval/cwiczenie_v_03_setTimeout_setInterval.js
@@ -1,36 +1,31 @@
 // setTimeout - metoda ktora wykonuje operacje po okreslonym czasie 
 // setInterval - metoda ktora wykonuje operacje co interwał (co okreslony przez nas czas w nieskonczoność)
 
-function stopWatch(wynik, liczba)
+const poczatkowaWartosc = document.querySelector("#poczatkowaWartosc");
+const statStoper = document.getElementById("start");
+const zatrzymajStoper = document.getElementById("stop");
+const wynik = document.getElementById("wynik");
+
+const stopWatch = (wynik, liczba) =>
 {
-    wynik.innerHTML = --liczba;
+    wynik.textContent = --liczba;
     if(liczba <= 0)
+    
     return
-    // to samo tylko pomniejszone o liczbe -
-    setTimeout(function()
+    setTimeout(()=>
     {
         stopWatch(wynik, liczba)
     },1000
     )
 }
 
-window.onload = function(){
-    var poczatkowaWartosc = document.querySelector("#poczatkowaWartosc");
-    var statStoper = document.getElementById("start");
-    var zatrzymajStoper = document.getElementById("stop");
-    var wynik = document.getElementById("wynik");
-
-    
-
-    statStoper.onclick = function(){
-        var poczatkowaWartosc = document.getElementById("poczatkowaWartosc").value;
-        wynik.innerHTML = poczatkowaWartosc;
-        setTimeout(function()
+    statStoper.onclick = () =>{
+        const poczatkowaWartosc = document.getElementById("poczatkowaWartosc").value;
+        wynik.textContent = poczatkowaWartosc;
+        setTimeout(()=>
         {
             stopWatch(wynik, poczatkowaWartosc)
         }
         ,1000);
-        
-    
     };
-}
+
