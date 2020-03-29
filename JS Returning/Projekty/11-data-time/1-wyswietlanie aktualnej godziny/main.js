a73e34e6d31efec7d9ae4bc33ff31756ca9d1d71
MrozKarol <polityk357@gmail.com>
2020-03-29 18:35:57 +0200

    fix the length change problem

--- JS Returning/Projekty/11-data-time/1-wyswietlanie aktualnej godziny/main.js
+++ JS Returning/Projekty/11-data-time/1-wyswietlanie aktualnej godziny/main.js
@@ -2,9 +2,9 @@ const clock = () => {
     const time = new Date();
     // console.log(time.toLocaleString());
     // console.log(time.toLocaleDateString());
-    const seconds = time.getSeconds();
-    const minutes = time.getMinutes()
-    const hours = time.getHours();
+    const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() <10 ? "0" + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() <10 ? "0" + time.getHours() : time.getHours();
 
     document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`;
 
