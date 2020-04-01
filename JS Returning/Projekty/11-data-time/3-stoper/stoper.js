93f123cb64a89283e1c8f1995f56e2ce6123a587
MrozKarol <polityk357@gmail.com>
2020-04-01 16:46:39 +0200

    displaying seconds after pressing the start button

--- JS Returning/Projekty/11-data-time/3-stoper/stoper.js
+++ JS Returning/Projekty/11-data-time/3-stoper/stoper.js
@@ -7,16 +7,21 @@ let active = false
 
 const stoper = () => {
     btnTime.classList.toggle('active')
-    if(!active){
+    if (!active) {
         active = !active;
-        btnTime.textContent = 'Pauza'
-    }else{
+        btnTime.textContent = 'Pauza';
+    } else {
         active = !active
-        btnTime.textContent = 'Start'
+        btnTime.textContent = 'Start';
+        clearInterval(start)
     }
     console.log(active)
 }
 
+const start = () => {
+    time++;
+    result.textContent = (time / 100).toFixed(2)
+}
 
 
 btnTime.addEventListener('click', stoper);
