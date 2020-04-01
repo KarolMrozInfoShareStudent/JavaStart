a6dfc144ddf78a0926c265d4b0dd1fef0ab26a7f
MrozKarol <polityk357@gmail.com>
2020-04-01 16:52:12 +0200

    added stopwatch pause

--- JS Returning/Projekty/11-data-time/3-stoper/stoper.js
+++ JS Returning/Projekty/11-data-time/3-stoper/stoper.js
@@ -3,17 +3,19 @@ const btnreset = document.querySelector('reset')
 const result = document.querySelector('.time div')
 
 let time = 0;
-let active = false
+let active = false;
+let idI; //zadeklarowanie zmiennej której zadaniem jest uzyskanie id funkcji setInterval(start,10)
 
 const stoper = () => {
     btnTime.classList.toggle('active')
     if (!active) {
         active = !active;
         btnTime.textContent = 'Pauza';
+        idI = setInterval(start ,10) //przypisanie do zmiennej Id funkcji setInterval
     } else {
         active = !active
         btnTime.textContent = 'Start';
-        clearInterval(start)
+        clearInterval(idI)
     }
     console.log(active)
 }
