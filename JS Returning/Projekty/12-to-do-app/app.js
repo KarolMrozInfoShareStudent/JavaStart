afe5b9e1193736fd675120f5a0de43c7a3f580d8
MrozKarol <polityk357@gmail.com>
2020-04-07 20:09:52 +0200

    task removal after clicking the universal solution

--- JS Returning/Projekty/12-to-do-app/app.js
+++ JS Returning/Projekty/12-to-do-app/app.js
@@ -1,10 +1,14 @@
 const removeTask = (e) => {
     // e.target.remove();
     // e.target.parentNode.remove();
-    e.target.parentNode.style.textDecoration = 'line-through'
-    e.target.remove()
+    // e.target.parentNode.style.textDecoration = 'line-through'
+    // e.target.remove()
+    const index = e.target.dataset.key;
+    console.log(index);
+    document.querySelector(`li[data-key="${index}"]`).remove()
+
 }
 
-document.querySelectorAll('li button').forEach(item => item.addEventListener('click', removeTask));
+document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask));
 
 
