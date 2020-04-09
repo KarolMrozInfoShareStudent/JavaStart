56601672aac7aa647fa467b9b5c4b220f9de659c
MrozKarol <polityk357@gmail.com>
2020-04-09 20:49:45 +0200

    reminder how the map function works

--- JS Returning/Teroria/Metody/app.js
+++ JS Returning/Teroria/Metody/app.js
@@ -1,6 +1,6 @@
 //Filter
 
-const arr = [33,35,67,100,21,22,25,88,89,1000,343,567,320];
+const arr = [33, 35, 67, 100, 21, 22, 25, 88, 89, 1000, 343, 567, 320];
 
 const oddNumbers = arr.filter(number => number % 2)
 console.log(oddNumbers);
@@ -9,4 +9,13 @@ const evenNumber = arr.filter(number => !(number % 2));
 console.log(evenNumber);
 
 const numberBiggerThen100 = arr.filter(number => number > 100);
-console.log(numberBiggerThen100)
+console.log(numberBiggerThen100);
+
+// MAP
+
+const double = arr.map(number => number * 2);
+console.log(double);
+
+const people = arr.map(number => number + ' osób');
+console.log(people)
+
