e59a1c7e572e39c25c59b4871d62a47aa96e54a7
MrozKarol <polityk357@gmail.com>
2020-05-05 18:37:19 +0200

    download application elements

---
+++ JS Returning/Projekty/12-to-do-app/3-add task/app.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h1 span');
+const listItem = document.querySelectorAll('li');
+
