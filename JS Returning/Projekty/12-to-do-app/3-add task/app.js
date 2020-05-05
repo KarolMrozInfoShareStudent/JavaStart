4c3e9fb12d2132b73c8a9af367082e617daa5ffb
MrozKarol <polityk357@gmail.com>
2020-05-05 18:51:20 +0200

    adding task and delete button, button not work yet

--- JS Returning/Projekty/12-to-do-app/3-add task/app.js
+++ JS Returning/Projekty/12-to-do-app/3-add task/app.js
@@ -1,5 +1,24 @@
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
-const listItem = document.querySelectorAll('li');
+const listItem = document.querySelectorAll('li.task');
+const input = document.querySelector('input')
 
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    // console.log(titleTask);
+
+    if (titleTask === '') return
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + '<button>Delate</button>';
+    ul.appendChild(task);
+    input.value = '';
+    ;
+
+
+}
+
+
+form.addEventListener('submit', addTask)
