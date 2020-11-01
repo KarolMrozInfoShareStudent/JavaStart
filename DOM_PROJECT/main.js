d5d736d453572f943f3a05fced576ea24e00e301
MrozKarol <polityk357@gmail.com>
2020-11-01 19:56:03 +0100

    adding element to the page

--- DOM_PROJECT/main.js
+++ DOM_PROJECT/main.js
@@ -1,12 +1,24 @@
-const addElement = (e, node = "div", txt = "text", attr = "style", value = "font-size:20px") => {
+const addElement = (e, node, txt, attr, value) => {
   e.preventDefault();
   console.log("submint");
   const element = document.createElement(node);
-  const text = document.createTextNode(txt);
-  element.appendChild(text);
-  element.setAttribute(attr,value);
-  document.body.appendChild(element)
+  if (txt) {
+    const text = document.createTextNode(txt);
+    element.appendChild(text);
+  }
+  if (attr) {
+    element.setAttribute(attr, value);
+    document.querySelector(".content").appendChild(element);
+  }
 };
 
 const addForm = document.querySelector(".form--add");
-addForm.addEventListener("submit", addElement);
+addForm.addEventListener("submit", (e) =>
+  addElement(
+    e,
+    addForm.elements.node.value,
+    addForm.elements.text.value,
+    addForm.elements.attr.value,
+    addForm.elements.value.value
+  )
+);
