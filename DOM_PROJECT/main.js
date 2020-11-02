905048925573cc267b412a7ec2df72d6f82f5908
MrozKarol <polityk357@gmail.com>
2020-11-02 19:41:07 +0100

    element information search engine added

--- DOM_PROJECT/main.js
+++ DOM_PROJECT/main.js
@@ -1,6 +1,6 @@
 const addElement = (e, node, txt, attr, value) => {
   e.preventDefault();
-  console.log("submint");
+  // console.log("submint");
   const element = document.createElement(node);
   if (txt) {
     const text = document.createTextNode(txt);
@@ -14,15 +14,33 @@ const addElement = (e, node, txt, attr, value) => {
 
 const searchElements = (e, nameElemnet) => {
   e.preventDefault();
-  console.log(nameElemnet);
   const infoELemnt = document.querySelector(".result");
+  infoELemnt.textContent = ''
   const elements = document.querySelectorAll(nameElemnet);
+  // console.log([...elements]);
   if (elements.length) {
     infoELemnt.innerHTML = `<p class='result_info'> W tym elemencie mamy ${elements.length} elementów ${nameElemnet}`;
+    showInfo(elements, infoELemnt);
   } else {
     infoELemnt.innerHTML = `<p class='result_info'> Nie znaleziono elementów o nazwie ${nameElemnet}`;
     return;
   }
+};
+
+const showInfo = (elements, infoELemnt) => {
+  console.log(elements, infoELemnt);
+  elements.forEach(elment =>{
+    const item = document.createElement('div');
+    item.className = 'element-info';
+    item.innerHTML = `
+    <div>${elment.nodeName}</div>
+    <div>klasa/klasy: ${elment.className}</div>
+    <div>Szerokość elementu: ${elment.offsetWidth}</div>
+    <div>Wysokość elementu: ${elment.offsetHeight}</div>
+    <div>Liczba elementów dzieci: ${elment.childElementCount}</div>
+    `
+    infoELemnt.appendChild(item)
+  })
 };
 
 //nasluchiwanie
