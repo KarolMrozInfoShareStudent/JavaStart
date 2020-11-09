a4a9ecd715a8109c859570fe7eba85bb3801cf6b
MrozKarol <polityk357@gmail.com>
2020-11-09 19:12:30 +0100

    fetch method, displaying users to the console using the json method

--- FETCH/1_FETCH/main.js
+++ FETCH/1_FETCH/main.js
@@ -1,8 +1,18 @@
 const getUsers = () => {
     const url = 'https://randomuser.me/api/?results=10';
     fetch(url)
-        .then((response) => console.log(response))
+        .then((response) => {
+            console.log(response)
+            if (response.status !== 200) {
+                alert("To nie jest odpowiedź 200")
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(json => console.log(json))
         .catch(err => console.log(err))
+
 }
 
 document.querySelector('button').addEventListener('click', getUsers);
