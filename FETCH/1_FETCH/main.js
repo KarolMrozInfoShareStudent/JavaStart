0301365509bfcf1463c40dce45db47b007ab58ac
MrozKarol <polityk357@gmail.com>
2020-11-10 19:38:01 +0100

    Form and parameter creation according to API

--- FETCH/1_FETCH/main.js
+++ FETCH/1_FETCH/main.js
@@ -1,18 +1,24 @@
-const getUsers = () => {
-    const url = 'https://randomuser.me/api/?results=10';
-    fetch(url)
-        .then((response) => {
-            console.log(response)
-            if (response.status !== 200) {
-                alert("To nie jest odpowiedź 200")
-            }
-            else {
-                return response.json()
-            }
-        })
-        .then(json => console.log(json))
-        .catch(err => console.log(err))
+const getUsers = (e) => {
+  e.preventDefault();
 
-}
+  const usersNumber = document.querySelector('[name = "users-number"]').value;
+  const usersGender = document.querySelector('[name = "gender"]').value;
+  //   console.log(usersNumber, usersGender);
 
-document.querySelector('button').addEventListener('click', getUsers);
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${
+    usersGender === "both" ? "male,femle" : usersGender
+  }`;
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+      if (response.status !== 200) {
+        alert("To nie jest odpowiedź 200");
+      } else {
+        return response.json();
+      }
+    })
+    .then((json) => console.log(json))
+    .catch((err) => console.log(err));
+};
+
+document.querySelector(".generator").addEventListener("submit", getUsers);
