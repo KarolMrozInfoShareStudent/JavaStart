4c48fef6818f509b444a9493ad175c55e416df36
MrozKarol <polityk357@gmail.com>
2020-11-30 20:50:37 +0100

    added remove definition single name

--- JS Returning/Projekty/6-losowanie/3-dodanie elementu do tablicy/main.js
+++ JS Returning/Projekty/6-losowanie/3-dodanie elementu do tablicy/main.js
@@ -1,21 +1,46 @@
 const names = [];
-const btn = document.querySelector('button')
-const div = document.querySelector('div')
+const btn = document.querySelector("button");
+const btnRemove = document.querySelector(".btnRemove");
+const div = document.querySelector("div");
+const input = document.querySelector("input");
 
 const addName = (e) => {
-    e.preventDefault();
-    const input = document.querySelector('input')
-    const newName = input.value
-    if (input.value.length) {
-        for (name of names){
-            if(newName === name){
-                alert('to już jest')
-                return
-            } 
-        }
-        names.push(newName)
-        div.textContent += newName + ', ';
-        input.value = '';
+  e.preventDefault();
+  const newName = input.value;
+  if (input.value.length) {
+    for (name of names) {
+      if (newName === name) {
+        alert("to już jest");
+        return;
+      }
     }
-}
-btn.addEventListener('click', addName)
+    names.push(newName);
+    // div.textContent += newName + ", ";
+    div.textContent = names;
+    input.value = "";
+  }
+};
+
+const removeName = (e) => {
+  e.preventDefault();
+  const removeName = input.value;
+  if (removeName.length && input.value) {
+    for (name of names) {
+      if (removeName === name) {
+        console.log(names.indexOf(name));
+        names.splice(names.indexOf(name), 1);
+        console.log(names);
+        div.textContent = names;
+        input.value = "";
+      } else {
+        alert("brak dopasowania");
+      }
+    }
+  }
+  if (!names.length) {
+    alert("brak pozycji do usunięcia");
+  }
+};
+
+btn.addEventListener("click", addName);
+btnRemove.addEventListener("click", removeName);
